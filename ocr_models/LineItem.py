063df8c3d5654019fa70ca93233483af05d252ac
Shahad <shahd.sola143@ai.menofia.edu.eg>
2024-10-08 16:28:09 +0300

    Delete ocr_models directory

--- ocr_models/LineItem.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-class LineItem:
-    def __init__(self):
-        self.line_item_id: int = 0
-        self.invoice_id: Optional[int] = None
-        self.item_name: str = ""
-        self.item_qty: Optional[float] = None
-        self.item_amount: Optional[float] = None
--- ocr_models/Row.py
+++
@@ -1,6 +0,0 @@
-class Row:
-    def __init__(self):
-        self.line: int = 0
-        self.word: str = ""
-        self.column: int = 0
-        self.type: int = 0
